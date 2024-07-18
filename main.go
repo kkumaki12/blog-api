e831fbffbf7e6ea7d4b3b61180306d115d9ae839
kkumaki12 <k.kumaki12@gmail.com>
2024-07-19 00:40:13 +0900

    gollila/muxを使って許容するHTTPメソッドを指定する

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/hello", handlers.HelloHandler)
-	r.HandleFunc("/article", handlers.PostArticleHandler)
-	r.HandleFunc("/articles", handlers.ArticleListHandler)
-	r.HandleFunc("/articles/1", handlers.ArticleDetailHandler)
-	r.HandleFunc("/nice", handlers.PostNiceHandler)
-	r.HandleFunc("/comment", handlers.PostCommentHandler)
+	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/articles", handlers.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/articles/1", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
