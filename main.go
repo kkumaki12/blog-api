7e997120bbbdd68c18e50f744fc604596bc2670d
kkumaki12 <k.kumaki12@gmail.com>
2024-07-19 00:35:32 +0900

    gollila/muxの導入

--- main.go
+++ main.go
@@ -4,17 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/kkumaki12/blog-api/handlers"
 )
 
 func main() {
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/articles", handlers.ArticleListHandler)
-	http.HandleFunc("/articles/1", handlers.ArticleDetailHandler)
-	http.HandleFunc("/nice", handlers.PostNiceHandler)
-	http.HandleFunc("/comment", handlers.PostCommentHandler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", handlers.HelloHandler)
+	r.HandleFunc("/article", handlers.PostArticleHandler)
+	r.HandleFunc("/articles", handlers.ArticleListHandler)
+	r.HandleFunc("/articles/1", handlers.ArticleDetailHandler)
+	r.HandleFunc("/nice", handlers.PostNiceHandler)
+	r.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
