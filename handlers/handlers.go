5b35e14d2cc82794fa25cdc4468943b7a2f9bc0c
kkumaki12 <k.kumaki12@gmail.com>
2024-07-19 00:25:58 +0900

    許可するHTTPメソッドを指定する

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,27 +7,51 @@ import (
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello, world!\n")
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Hello, world!\n")
+	} else {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Post article\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Post article\n")
+	} else {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article list\n")
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Article list\n")
+	} else {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID := 1
-	resString := fmt.Sprintf("Article detail %d\n", articleID)
-	io.WriteString(w, resString)
+	if req.Method == http.MethodGet {
+		articleID := 1
+		resString := fmt.Sprintf("Article detail %d\n", articleID)
+		io.WriteString(w, resString)
+	} else {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Post nice\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Post nice\n")
+	} else {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Post comment\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Post comment\n")
+	} else {
+		http.Error(w, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
