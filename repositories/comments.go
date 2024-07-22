d071078af06ce022b1f44e5271ea677ef96feeb5
kkumaki12 <k.kumaki12@gmail.com>
2024-07-22 19:13:04 +0900

    comments.goの実装

---
+++ repositories/comments.go
@@ -0,0 +1,55 @@
+package repositories
+
+import (
+	"database/sql"
+
+	"github.com/kkumaki12/blog-api/models"
+)
+
+func InsertComment(db *sql.DB, comment models.Comment) (models.Comment, error) {
+	const sqlStr = `
+		insert into comments (article_id, message, created_at) values
+		(?, ?, now());
+		`
+
+	var newComment models.Comment
+	newComment.ArticleID, newComment.Message = comment.ArticleID, comment.Message
+
+	result, err := db.Exec(sqlStr, comment.ArticleID, comment.Message)
+	if err != nil {
+		return models.Comment{}, err
+	}
+
+	id, _ := result.LastInsertId()
+	newComment.CommentID = int(id)
+	return newComment, nil
+}
+
+func selectCommentList(db *sql.DB, articleID int) ([]models.Comment, error) {
+	var commentArray []models.Comment
+	const sqlStr = `
+		select comment_id, message, created_at
+		from comments
+		where article_id = ?;
+	`
+
+	rows, err := db.Query(sqlStr, articleID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var comment models.Comment
+		var createdTime sql.NullTime
+		rows.Scan(&comment.CommentID, &comment.Message, &createdTime)
+
+		if createdTime.Valid {
+			comment.CreatedAt = createdTime.Time
+		}
+
+		commentArray = append(commentArray, comment)
+	}
+
+	return commentArray, nil
+}
