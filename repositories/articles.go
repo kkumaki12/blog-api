9a22f9d774823d05aff29d3930d6fe77815c09a7
kkumaki12 <k.kumaki12@gmail.com>
2024-07-22 18:18:05 +0900

    insertとselect 処理の実装

---
+++ repositories/articles.go
@@ -0,0 +1,55 @@
+package articles
+
+import (
+	"database/sql"
+
+	"github.com/kkumaki12/blog-api/models"
+)
+
+const (
+	articleNumPerPage = 5
+)
+
+func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
+	const sqlStr = `
+		insert into articles (title, contents, username, nice, created_at) values
+		(?, ?, ?, 0, now());
+		`
+
+	var newArticle models.Article
+	newArticle.Title, newArticle.Contents, newArticle.UserName = article.Title, article.Contents, article.UserName
+
+	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	id, _ := result.LastInsertId()
+	newArticle.ID = int(id)
+	return newArticle, nil
+}
+
+func SelectArticleList(db *sql.DB, page int) ([]models.Article, error) {
+	const sqlStr = `
+		select article_id, title, contents, username, nice
+		from articles
+		limit ? offset ?;
+	`
+
+	var articleArray []models.Article
+
+	rows, err := db.Query(sqlStr, articleNumPerPage, (page-1)*articleNumPerPage)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var article models.Article
+		rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+
+		articleArray = append(articleArray, article)
+	}
+
+	return articleArray, nil
+}
