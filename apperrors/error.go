8b84dbf8c3c19fa7a08cd6650a3af4a6217b9fef
kkumaki12 <k.kumaki12@gmail.com>
2024-07-26 15:13:49 +0900

    エラーチェーン

--- apperrors/error.go
+++ apperrors/error.go
@@ -3,8 +3,13 @@ package apperrors
 type MyAppError struct {
 	ErrCode
 	Message string
+	Err     error // エラーチェーンのための内部エラー
 }
 
 func (myErr *MyAppError) Error() string {
 	return myErr.Message
+}
+
+func (myErr *MyAppError) Unwrap() error {
+	return myErr.Err
 }
