a92afb7d0dbf67a4366e6ba99cfc89b77ce1647a
kkumaki12 <k.kumaki12@gmail.com>
2024-07-27 10:23:00 +0900

    モックを使用してテストする

--- controllers/main_test.go
+++ controllers/main_test.go
@@ -1,14 +1,10 @@
 package controllers_test
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"os"
 	"testing"
 
 	"github.com/kkumaki12/blog-api/controllers"
-	"github.com/kkumaki12/blog-api/services"
+	"github.com/kkumaki12/blog-api/controllers/testdata"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -16,18 +12,7 @@ import (
 var aCon *controllers.ArticleController
 
 func TestMain(m *testing.M) {
-	dbUser := "docker"
-	dbPassword := "docker"
-	dbDatabase := "sampledb"
-	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
-
-	db, err := sql.Open("mysql", dbConn)
-	if err != nil {
-		log.Println("db connection error")
-		os.Exit(1)
-	}
-
-	ser := services.NewMyAppService(db)
+	ser := testdata.NewServiceMock()
 	aCon = controllers.NewArticleController(ser)
 
 	m.Run()
